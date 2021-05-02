e6dac87645da3a070fccca82f6ec2bcc95d243ef
Debashis Ray <debaray14@gmail.com>
2021-05-02 16:06:39 +0600

    Blog done

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import NavBar from '../Shared/NavBar/NavBar';
-
+import './Blogs.css'
 const Blogs = () => {
     return (
-        <div>
-            
-            <h1>This is Blogs</h1>
+        <div className="container">
+            <h2 className="text-center blog-text-style">Blogs</h2>
+            <hr/>
+            <h2 className="text-center blog-text-style">Blogs are Coming Soon...</h2>
         </div>
     );
 };
