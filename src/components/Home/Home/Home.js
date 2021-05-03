1968b17eff59b14052f8a721742ad5d504b64c39
Debashis Ray <debaray14@gmail.com>
2021-05-03 17:23:40 +0600

    update home

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Header from '../Header/Header';
 import HomeProjects from '../HomeProjects/HomeProjects';
 import HomeSkills from '../HomeSkills/HomeSkills';
-
+import ContactMe from '../../ContactMe/ContactMe';
 const Home = () => {
     return (
         <div>
             <Header></Header>
             <HomeSkills></HomeSkills>
             <HomeProjects></HomeProjects>
+            <ContactMe></ContactMe>
            
         </div>
     );
